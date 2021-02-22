e2e23b507bc5bf059b057b3e74fb09e6f9eabff5
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-02-22 14:10:57 -0600

    initial commit

---
+++ validatingFormInput/src/main/java/com/tts/validatingFormInput/ValidatingFormInputApplication.java
@@ -0,0 +1,13 @@
+package com.tts.validatingFormInput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingFormInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingFormInputApplication.class, args);
+	}
+
+}
